a941fd90db9a5a76fe19308cd32d99e021807d54
hibanashid15 <hiba@claudion.com>
2022-06-21 05:30:40 +0000

    patient encounter customisation

--- patient_encounter_customisation/patient_encounter_customisation/doctype/healthcare_item/healthcare_item.js
+++ patient_encounter_customisation/patient_encounter_customisation/doctype/healthcare_item/healthcare_item.js
@@ -2,7 +2,7 @@ frappe.ui.form.on('Healthcare Item', "item_code",function(frm,cdt,cdn)
 {
     let item=locals[cdt][cdn]
     let item_code=item.item_code;
-    console.log(item_code)
+    
     if(item_code){
        frappe.call({
 	          method:"patient_encounter_customisation.patient_encounter_customisation.doctype.healthcare_item.healthcare_item.get_item_detail",
@@ -13,12 +13,14 @@ frappe.ui.form.on('Healthcare Item', "item_code",function(frm,cdt,cdn)
 	          item.description=r.message[0][0].description
 	          item.uom=r.message[0][0].stock_uom
 	          item.conversion_factor=1
-			  
-	          item.rate=r.message[0][0].valuation_rate
-	          let item_qty=item.qty;
-	          let item_rate=item.rate;
+			  item.price_list_rate=r.message[0][1][0].price_list_rate
+	          item.rate=r.message[0][1][0].price_list_rate
+              let amount=r.message[0][1][0].price_list_rate
+              console.log(item.margin_type)
+	        
+              
 	          refresh_field("items")
-	         console.log(item_qty)
+	         
 	   
 	      })
     }
@@ -27,12 +29,27 @@ frappe.ui.form.on('Healthcare Item', "item_code",function(frm,cdt,cdn)
 frappe.ui.form.on('Healthcare Item', "qty",function(frm,cdt,cdn)
 {
     let item=locals[cdt][cdn]
-    console.log(item.qty)
+    
     item.amount=(item.qty)*(item.rate)
+    item.discount_amount=0.0
+    
      refresh_field("items")
 })
 
-
+frappe.ui.form.on('Healthcare Item', "discount_percentage",function(frm,cdt,cdn)
+{
+    let item=locals[cdt][cdn]
+    
+    item.amount=(item.qty)*(item.rate)
+    item.discount_amount=0.0
+    item.discount_amount=item.rate*(item.discount_percentage/100)
+    
+    item.rate=item.rate-item.discount_amount
+    item.amount=(item.qty)*(item.rate)
+    refresh_field("items")
+    
+    
+})
 
 frappe.ui.form.on("Healthcare Item", {
         qty:function(frm, cdt, cdn){
@@ -50,14 +67,54 @@ frappe.ui.form.on("Healthcare Item", {
         refresh_field("grand_total");
             }
         });
-
+frappe.ui.form.on("Healthcare Item", {
+            discount_percentage:function(frm, cdt, cdn){
+            var d = locals[cdt][cdn];
+            var total = 0;
+            frm.doc.items.forEach(function(d) { total += d.amount; });
+            frm.set_value("grand_total", total);
+            refresh_field("grand_total");
+          },
+            items_remove:function(frm, cdt, cdn){
+            var d = locals[cdt][cdn];
+            var total = 0;
+            frm.doc.items.forEach(function(d) { total += d.amount; });
+            frm.set_value("grand_total", total);
+            refresh_field("grand_total");
+                }
+            });
+frappe.ui.form.on("Healthcare Item", {
+            qty:function(frm, cdt, cdn){
+            var d = locals[cdt][cdn];
+            var total = 0;
+            frm.doc.items.forEach(function(d) { 
+                refresh_field("amount");
+                total += d.amount; });
+            frm.set_value("grand_total", total);
+            refresh_field("grand_total");
+          },
+            items_remove:function(frm, cdt, cdn){
+            var d = locals[cdt][cdn];
+            var total = 0;
+            frm.doc.items.forEach(function(d) { total += d.amount; });
+            frm.set_value("grand_total", total);
+            refresh_field("grand_total");
+                }
+            });
 frappe.ui.form.on("Sales Invoice","onload", function(frm){
+    
             frm.set_query("patient", function() {
                 return {
                     query: "patient_encounter_customisation.patient_encounter_customisation.doctype.healthcare_item.healthcare_item.filter_patients"
                     
                 };
             });
+            })
+frappe.ui.form.on("Sales Invoice","refresh", function(frm){
+                if (frappe.boot.active_domains.includes("Healthcare")){
+                frm.add_custom_button(__('Patient Encounter Items'), function() {
+                    get_drugs_to_invoice(frm);
+                },__("Fetch items From"));  } 
             })
 frappe.ui.form.on("Sales Invoice", {
                 on_submit: function(frm) {
@@ -70,3 +127,83 @@ frappe.ui.form.on("Sales Invoice", {
                 })
                 }
               });
+
+var get_drugs_to_invoice = function(frm) {
+                var me = this;
+                let selected_encounter = '';
+                var dialog = new frappe.ui.Dialog({
+                    title: __("Get Items from Prescriptions"),
+                    fields:[
+                        { fieldtype: 'Link', options: 'Patient', label: 'Patient', fieldname: "patient", reqd: true },
+                        { fieldtype: 'Link', options: 'Patient Encounter', label: 'Patient Encounter', fieldname: "encounter", reqd: true,
+                            description:'Quantity will be calculated only for items which has "Nos" as UoM. You may change as required for each invoice item.',
+                            get_query: function(doc) {
+                                return {
+                                    filters: {
+                                        patient: dialog.get_value("patient"),
+                                        company: frm.doc.company,
+                                        docstatus: 1
+                                    }
+                                };
+                            }
+                        },
+                        { fieldtype: 'Section Break' },
+                        { fieldtype: 'HTML', fieldname: 'results_area' }
+                    ]
+                });
+                var $wrapper;
+                var $results;
+                var $placeholder;
+                dialog.set_values({
+                    'patient': frm.doc.patient,
+                    'encounter': ""
+                });
+                dialog.fields_dict["encounter"].df.onchange = () => {
+                    var encounter = dialog.fields_dict.encounter.input.value;
+                    if(encounter && encounter!=selected_encounter){
+                        selected_encounter = encounter;
+                        var method = "patient_encounter_customisation.patient_encounter_customisation.doctype.healthcare_item.healthcare_item.get_items_to_invoice";
+                        var args = {encounter: encounter};
+                        var columns = (["drug_code", "quantity", "description"]);
+                        get_healthcare_items(frm, false, $results, $placeholder, method, args, columns);
+                    }
+                    else if(!encounter){
+                        selected_encounter = '';
+                        $results.empty();
+                        $results.append($placeholder);
+                    }
+                }
+                $wrapper = dialog.fields_dict.results_area.$wrapper.append(`<div class="results"
+                    style="border: 1px solid #d1d8dd; border-radius: 3px; height: 300px; overflow: auto;"></div>`);
+                $results = $wrapper.find('.results');
+                $placeholder = $(`<div class="multiselect-empty-state">
+                            <span class="text-center" style="margin-top: -40px;">
+                                <i class="fa fa-2x fa-heartbeat text-extra-muted"></i>
+                                <p class="text-extra-muted">No Drug Prescription found</p>
+                            </span>
+                        </div>`);
+                $results.on('click', '.list-item--head :checkbox', (e) => {
+                    $results.find('.list-item-container .list-row-check')
+                        .prop("checked", ($(e.target).is(':checked')));
+                });
+                set_primary_action(frm, dialog, $results, false);
+                dialog.show();
+            };
+var get_healthcare_items = function(frm, invoice_healthcare_services, $results, $placeholder, method, args, columns) {
+                var me = this;
+                $results.empty();
+                frappe.call({
+                    method: method,
+                    args: args,
+                    callback: function(data) {
+                        if(data.message){
+                            $results.append(make_list_row(columns, invoice_healthcare_services));
+                            for(let i=0; i<data.message.length; i++){
+                                $results.append(make_list_row(columns, invoice_healthcare_services, data.message[i]));
+                            }
+                        }else {
+                            $results.append($placeholder);
+                        }
+                    }
+                });
+            }
--- patient_encounter_customisation/patient_encounter_customisation/doctype/healthcare_item/healthcare_item.py
+++ patient_encounter_customisation/patient_encounter_customisation/doctype/healthcare_item/healthcare_item.py
@@ -4,14 +4,17 @@
 import frappe
 from frappe.model.document import Document
 from datetime import date
+from erpnext.stock.get_item_details import get_item_details
 
 class HealthcareItem(Document):
 	pass
 @frappe.whitelist()
-def get_item_detail(item_code):
+def get_item_detail(item_code='Kratom'):
     item_details=[]
     item_detail=frappe.db.sql(f"""SELECT item_name,stock_uom,valuation_rate,description FROM `tabItem` WHERE item_code='{item_code}';""",as_dict=True)
-    
+   
+    price_list_rate=frappe.db.sql(f"""SELECT  price_list_rate FROM `tabItem Price` WHERE item_code='{item_code}';""",as_dict=True)
+    item_detail.append(price_list_rate)
     item_details.append(item_detail)
     return item_details
 
@@ -26,3 +29,28 @@ def filter_patients(doctype, txt, searchfield, start, page_len, filters):
 def set_patient_status(patient_name):
     status=frappe.db.sql(f"""UPDATE `tabPatient Encounter` SET invoiced=1 WHERE patient='{patient_name}';""")
     return "done"
+   
+   
+@frappe.whitelist()
+def get_items_to_invoice(encounter):
+	encounter = frappe.get_doc("Patient Encounter", encounter)
+	if encounter:
+		patient = frappe.get_doc("Patient", encounter.patient)
+		if patient:
+			if patient.customer:
+				items_to_invoice = []
+				for drug_line in encounter.items:
+					if drug_line.item_code:
+						qty = 1
+						if frappe.db.get_value("Item", drug_line.item_code, "stock_uom") == "Nos":
+							qty = drug_line.qty
+
+						description = drug_line.description
+						
+
+						items_to_invoice.append(
+							{"drug_code": drug_line.item_code, "quantity": qty, "description": description}
+						)
+				return items_to_invoice
+			else:
+				validate_customer_created(patient)
